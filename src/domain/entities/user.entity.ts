84ad87d1b8542be59b15f58c76d135a6dc2b78f0
victorsz08 <joaovictorsal777@gmail.com>
2025-07-21 10:08:37 -0300

    feat: add user entity class

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,9 +1,14 @@
+import { Role } from "domain/enum/role.enum"
+import { DatePattern } from "patterns/date"
+import { Hash } from "patterns/hash"
+import { RandomId } from "patterns/random-id"
+
 export type User = {
     id: string
     username: string
     firstName: string
     lastName: string
-    role: string
+    role: Role
     password: string
     createdAt: Date
     updatedAt: Date
@@ -12,5 +17,57 @@ export type User = {
 export class UserEntity {
     private constructor(private readonly props: User) {}
 
-    public static build() {}
+    public static async build(
+        username: string,
+        firstName: string,
+        lastName: string,
+        password: string,
+    ) {
+        const id = RandomId.uuid()
+        const createdAt = DatePattern.getCurrentDate()
+        const updatedAt = DatePattern.getCurrentDate()
+        const passwordHash = await Hash.hash(password)
+
+        const user: User = {
+            id,
+            username,
+            firstName,
+            lastName,
+            role: Role.USER,
+            password: passwordHash,
+            createdAt,
+            updatedAt,
+        }
+
+        return new UserEntity(user)
+    }
+
+    public static with(props: User) {
+        return new UserEntity(props)
+    }
+
+    public get id(): string {
+        return this.props.id
+    }
+    public get username(): string {
+        return this.props.username
+    }
+    public get firstName(): string {
+        return this.props.firstName
+    }
+    public get lastName(): string {
+        return this.props.lastName
+    }
+    public get role(): Role {
+        return this.props.role
+    }
+    public get password(): string {
+        return this.props.password
+    }
+    public get createdAt(): Date {
+        return this.props.createdAt
+    }
+    public get updatedAt(): Date {
+        return this.props.updatedAt
+    }
 }
