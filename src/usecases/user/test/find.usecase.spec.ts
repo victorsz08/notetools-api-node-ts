e7021652c277392b8cd331b2b1e84df036c99640
victorsz08 <joaovictorsal777@gmail.com>
2025-07-21 12:38:54 -0300

    feat: add find user useacase test

---
+++ src/usecases/user/test/find.usecase.spec.ts
@@ -0,0 +1,56 @@
+import { UserInterface } from "@/domain/interface/user.interface"
+import { FindUserInput, FindUserOutput, FindUserUsecase } from "../find.usecase"
+import { UserEntity } from "@/domain/entities/user.entity"
+import { HttpException } from "@/package/exceptions/http-exceptions"
+
+describe("FindUserUsecase", () => {
+    let mockUserRepository: jest.Mocked<UserInterface>
+    let usecase: FindUserUsecase
+
+    beforeEach(() => {
+        mockUserRepository = {
+            create: jest.fn(),
+            find: jest.fn(),
+            findByUsername: jest.fn(),
+            list: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+            changePassword: jest.fn(),
+        }
+        usecase = FindUserUsecase.build(mockUserRepository)
+    })
+
+    it("deve buscar um usuário pelo id", async () => {
+        const user = await UserEntity.build("teste", "teste", "teste", "teste")
+        mockUserRepository.find.mockResolvedValue(user)
+
+        const input: FindUserInput = {
+            id: user.id,
+        }
+
+        const result = await usecase.execute(input)
+        const outputUser: FindUserOutput = {
+            id: user.id,
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            role: user.role,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+        }
+        expect(result).toEqual(outputUser)
+        expect(mockUserRepository.find).toHaveBeenCalledWith(input.id)
+    })
+
+    it("deve lançar um erro se o usuário não for encontrado", async () => {
+        mockUserRepository.find.mockResolvedValue(null)
+
+        const input: FindUserInput = {
+            id: "123",
+        }
+
+        await expect(usecase.execute(input)).rejects.toBeInstanceOf(
+            HttpException,
+        )
+    })
+})
