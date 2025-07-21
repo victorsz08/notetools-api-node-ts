8737656dadb87efd706f7c9f5a4d8c95c004c787
victorsz08 <joaovictorsal777@gmail.com>
2025-07-21 10:22:23 -0300

    feat: add http exception class pattern

---
+++ src/package/exceptions/http-exceptions.ts
@@ -0,0 +1,18 @@
+export type HttpStatusCode = 200 | 201 | 400 | 401 | 403 | 404 | 409 | 500
+export const HttpStatusCode = {
+    OK: 200 as HttpStatusCode,
+    CREATED: 201 as HttpStatusCode,
+    BAD_REQUEST: 400 as HttpStatusCode,
+    UNAUTHORIZED: 401 as HttpStatusCode,
+    FORBIDDEN: 403 as HttpStatusCode,
+    NOT_FOUND: 404 as HttpStatusCode,
+    CONFLICT: 409 as HttpStatusCode,
+    INTERNAL_SERVER_ERROR: 500 as HttpStatusCode,
+} as const
+
+export class HttpException extends Error {
+    public statusCode: number
+    constructor(message: string, statusCode: number) {
+        ;(super(message), (this.statusCode = statusCode))
+    }
+}
