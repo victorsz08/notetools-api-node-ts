c63c13132643d916e513b6a2a1795edb838c0f4d
victorsz08 <joaovictorsal777@gmail.com>
2025-07-21 11:48:26 -0300

    feat: add delete usecase class

---
+++ src/usecases/user/delete.usecase.ts
@@ -0,0 +1,36 @@
+import { UserInterface } from "@/domain/interface/user.interface"
+import { Usecase } from "../usecase.core"
+import {
+    HttpException,
+    HttpStatusCode,
+} from "@/package/exceptions/http-exceptions"
+
+export type DeleteUserInput = {
+    id: string
+}
+
+export type DeleteUserOutput = void
+
+export class DeleteUserUsecase
+    implements Usecase<DeleteUserInput, DeleteUserOutput>
+{
+    private constructor(private readonly userRepository: UserInterface) {}
+
+    public static build(userRepository: UserInterface) {
+        return new DeleteUserUsecase(userRepository)
+    }
+
+    public async execute(input: DeleteUserInput): Promise<void> {
+        const { id } = input
+        const user = await this.userRepository.find(id)
+
+        if (!user)
+            throw new HttpException(
+                HttpStatusCode.NOT_FOUND,
+                "usuário não encontrado com esse id",
+            )
+        await this.userRepository.delete(id)
+
+        return
+    }
+}
