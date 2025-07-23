10709002b44b0ba0d49159a96bb52037b3543a36
victorsz08 <joaovictorsal777@gmail.com>
2025-07-23 10:52:11 -0300

    feat: delete group order usecase

---
+++ src/usecases/order/delete-group.usecase.ts
@@ -0,0 +1,25 @@
+import { OrderInterface } from "@/domain/interface/order.interface"
+import { Usecase } from "../usecase.core"
+
+export type DeleteGroupOrderInput = {
+    ids: string[]
+}
+
+export type DeleteGroupOrderOutput = void
+
+export class DeleteGroupOrderUsecase
+    implements Usecase<DeleteGroupOrderInput, DeleteGroupOrderOutput>
+{
+    private constructor(private readonly orderRepository: OrderInterface) {}
+
+    public static build(orderRepository: OrderInterface) {
+        return new DeleteGroupOrderUsecase(orderRepository)
+    }
+
+    public async execute(input: DeleteGroupOrderInput): Promise<void> {
+        const { ids } = input
+        await this.orderRepository.deleteGroup(ids)
+
+        return
+    }
+}
