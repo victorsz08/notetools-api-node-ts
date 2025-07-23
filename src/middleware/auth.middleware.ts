4fc257931ae8d6b2c17f11901a2db0d1870b526b
Victor Developer <victorszdeveloper@gmail.com>
2025-07-23 21:11:39 +0000

    feat: add auth middleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express"
+import { verify } from "jsonwebtoken"
+
+export function AuthMiddleware() {
+    return async (request: Request, response: Response, next: NextFunction) => {
+        const token = request.cookies["nt.authtoken"]
+        if (!token) {
+            return response
+                .status(401)
+                .send({ message: "Token não localizado" })
+        }
+
+        try {
+            verify(token, String(process.env.JWT_SECRET))
+            next()
+        } catch (error) {
+            return response
+                .status(401)
+                .send({ message: "Usuário não autenticado", error: error })
+        }
+    }
+}
