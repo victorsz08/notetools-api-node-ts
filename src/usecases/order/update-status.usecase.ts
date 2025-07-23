9eb7790a931dd9e92285edd8f5d119d53540b9a7
victorsz08 <joaovictorsal777@gmail.com>
2025-07-23 09:54:46 -0300

    feat: add update status order usecase

---
+++ src/usecases/order/update-status.usecase.ts
@@ -0,0 +1,40 @@
+import { Status } from "@/domain/enum/status.enum"
+import { Usecase } from "../usecase.core"
+import { OrderInterface } from "@/domain/interface/order.interface"
+import {
+    HttpException,
+    HttpStatusCode,
+} from "@/package/exceptions/http-exceptions"
+
+export type UpdateStatusOrderInput = {
+    id: string
+    status: Status
+}
+
+export type UpdateStatusOrderOutput = void
+
+export class UpdateStatusOrderUsecase
+    implements Usecase<UpdateStatusOrderInput, UpdateStatusOrderOutput>
+{
+    private constructor(private readonly orderRepository: OrderInterface) {}
+
+    public static build(orderRepository: OrderInterface) {
+        return new UpdateStatusOrderUsecase(orderRepository)
+    }
+
+    public async execute(input: UpdateStatusOrderInput): Promise<void> {
+        const { id, status } = input
+        const order = await this.orderRepository.find(id)
+
+        if (!order) {
+            throw new HttpException(
+                HttpStatusCode.NOT_FOUND,
+                "Pedido não encontrado com esse id",
+            )
+        }
+
+        await this.orderRepository.updateStatus(id, status)
+
+        return
+    }
+}
