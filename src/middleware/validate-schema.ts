bf720650cf71525cece0b72577a85bc5333b481a
Victor Developer <victorszdeveloper@gmail.com>
2025-07-23 22:33:43 +0000

    feat: add validate schema zod middleware

---
+++ src/middleware/validate-schema.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { ZodError, z } from "zod"
+import { type Request, type Response, type NextFunction } from "express"
+
+type T = Record<any, any>
+export function Validation(schema: z.ZodObject<T>) {
+    return async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            const validateToData = request.body
+            schema.parse(validateToData)
+
+            next()
+        } catch (error) {
+            if (error instanceof ZodError) {
+                const errorMessage = error.issues.map((issue) => {
+                    return {
+                        path: issue.path,
+                        message: issue.message,
+                    }
+                })
+
+                return response.status(401).send({
+                    message: "Campos preenchidos incorretamentes",
+                    errors: errorMessage,
+                })
+            }
+
+            return response
+                .status(500)
+                .send({ message: "erro interno do servidor" })
+        }
+    }
+}
