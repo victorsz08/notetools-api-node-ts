10cb206443416d787a339ccbba72797e2bf9a664
Victor Developer <victorszdeveloper@gmail.com>
2025-07-24 19:31:01 +0000

    feat: add create note usecase

---
+++ src/usecases/note/create.usecase.ts
@@ -0,0 +1,29 @@
+import { NoteInterface } from "@/domain/interface/note.interface"
+import { Usecase } from "../usecase.core"
+import { NoteEntity } from "@/domain/entities/note.entity"
+
+export type CreateNoteInput = {
+    title: string
+    content: string
+    userId: string
+}
+
+export type CreateNoteOutput = void
+
+export class CreateNoteUsecase
+    implements Usecase<CreateNoteInput, CreateNoteOutput>
+{
+    private constructor(private readonly noteRepository: NoteInterface) {}
+
+    public static build(noteRepository: NoteInterface) {
+        return new CreateNoteUsecase(noteRepository)
+    }
+
+    public async execute(input: CreateNoteInput): Promise<void> {
+        const { title, content, userId } = input
+        const note = NoteEntity.build(title, content, userId)
+
+        await this.noteRepository.create(note)
+        return
+    }
+}
