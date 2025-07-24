45bb1633e00c20478548f648a0496d0e2df753ce
Victor Developer <victorszdeveloper@gmail.com>
2025-07-24 21:05:13 +0000

    feat: add list note usecase

---
+++ src/usecases/note/list.usecase.ts
@@ -0,0 +1,56 @@
+import { NoteInterface, NoteList } from "@/domain/interface/note.interface"
+import { Usecase } from "../usecase.core"
+
+export type ListNoteInput = {
+    page: number
+    limit: number
+    userId: string
+}
+
+export type ListNoteOutput = {
+    notes: {
+        id: string
+        title: string
+        content: string
+        createdAt: Date
+        updatedAt: Date
+    }[]
+    total: number
+    page: number
+    limit: number
+    totalPages: number
+}
+
+export class ListNoteUsecase implements Usecase<ListNoteInput, ListNoteOutput> {
+    private constructor(private readonly noteRepository: NoteInterface) {}
+
+    public static build(noteRepository: NoteInterface) {
+        return new ListNoteUsecase(noteRepository)
+    }
+
+    public async execute(input: ListNoteInput): Promise<ListNoteOutput> {
+        const { page, limit, userId } = input
+        const data = await this.noteRepository.list(page, limit, userId)
+
+        const output = this.present(data)
+        return output
+    }
+
+    private present(data: NoteList): ListNoteOutput {
+        return {
+            notes: data.notes.map((note) => {
+                return {
+                    id: note.id,
+                    title: note.title,
+                    content: note.content,
+                    createdAt: note.createdAt,
+                    updatedAt: note.updatedAt,
+                }
+            }),
+            total: data.total,
+            page: data.page,
+            limit: data.limit,
+            totalPages: data.totalPages,
+        }
+    }
+}
