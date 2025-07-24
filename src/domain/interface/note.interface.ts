fd2673e7d705f40fcb563ec484efc88a081d4b99
Victor Developer <victorszdeveloper@gmail.com>
2025-07-24 19:23:53 +0000

    feat: add note interface repository

---
+++ src/domain/interface/note.interface.ts
@@ -0,0 +1,22 @@
+import { NoteEntity } from "../entities/note.entity"
+
+export type NoteList = {
+    notes: NoteEntity[]
+    total: number
+    page: number
+    limit: number
+    totalPages: number
+}
+
+export interface NoteInterface {
+    create(note: NoteEntity): Promise<void>
+    find(id: string): Promise<NoteEntity>
+    list(page: number, limit: number, userId: string): Promise<NoteList>
+    update(
+        id: string,
+        title: string,
+        content: string,
+        updatedAt: Date,
+    ): Promise<void>
+    delete(id: string): Promise<void>
+}
