292040f22f6dcddad00867daa9b1fc17514a4bdb
victorsz08 <joaovictorsal777@gmail.com>
2025-07-25 12:43:23 -0300

    feat: add order validation dtos

---
+++ src/package/dtos/order.dto.ts
@@ -0,0 +1,37 @@
+import { Status } from "@/domain/enum/status.enum"
+import { TimeSlot } from "@/domain/enum/time-slot.enum"
+import { TypeOrder } from "@/domain/enum/type-order.enum"
+import z from "zod"
+
+export const createOrderDto = z.object({
+    number: z.coerce
+        .number()
+        .min(1, "O numero do pedido deve ser maior que zero"),
+    local: z.string().nonempty("O campo local é obrigatório"),
+    observation: z.string().optional().default("Nenhuma observação"),
+    schedulingDate: z.coerce.date(),
+    schedulingTime: z.enum(TimeSlot),
+    contact: z.string().nonempty("O campo contato é obrigatório"),
+    price: z.coerce.number().min(0, "O preço deve ser maior ou igual a zero"),
+    type: z.enum(TypeOrder),
+})
+
+export const updateUserDto = z.object({
+    number: z.coerce
+        .number()
+        .min(1, "O numero do pedido deve ser maior que zero"),
+    local: z.string().nonempty("O campo local é obrigatório"),
+    observation: z.string().optional().default("Nenhuma observação"),
+    contact: z.string().nonempty("O campo contato é obrigatório"),
+    price: z.coerce.number().min(0, "O preço deve ser maior ou igual a zero"),
+    type: z.enum(TypeOrder),
+})
+
+export const updateSchedulingOrderDto = z.object({
+    schedulingDate: z.coerce.date(),
+    schedulingTime: z.enum(TimeSlot),
+})
+
+export const updateStatusOrderDto = z.object({
+    status: z.enum(Status),
+})
