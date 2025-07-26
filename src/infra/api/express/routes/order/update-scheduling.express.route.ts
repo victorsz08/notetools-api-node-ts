39ca0b22dff23ecacf44aeaedb87276f0d641ceb
victorsz08 <joaovictorsal777@gmail.com>
2025-07-26 06:46:04 -0300

    feat: add update scheduling order express route

--- src/infra/api/express/routes/order/update-scheduling.express.route.ts
+++ src/infra/api/express/routes/order/update-scheduling.express.route.ts
@@ -1,32 +1,31 @@
-import { UpdateOrderUsecase } from "@/usecases/order/update.usecase"
-import { HttpMethod, Route } from "../route.express"
 import { Request, Response, NextFunction } from "express"
+import { HttpMethod, Route } from "../route.express"
+import { UpdateSchedulingOrderUsecase } from "@/usecases/order/update-scheduling.usecase"
 import { AuthMiddleware } from "@/middleware/auth.middleware"
 import { Validation } from "@/middleware/validate-schema"
-import { updateOrderDto } from "@/package/dtos/order.dto"
+import { updateSchedulingOrderDto } from "@/package/dtos/order.dto"
 
-export class UpdateOrderRoute implements Route {
+export class UpdateSchedulingOrderRoute implements Route {
     private constructor(
         private readonly path: string,
         private readonly method: HttpMethod,
-        private readonly updateOrderUsecase: UpdateOrderUsecase,
+        private readonly updateSchedulingOrderUsecase: UpdateSchedulingOrderUsecase,
     ) {}
 
-    public static build(updateOrderUsecase: UpdateOrderUsecase) {
-        return new UpdateOrderRoute(
-            "/orders/:id",
+    public static build(
+        updateSchedulingOrderUsecase: UpdateSchedulingOrderUsecase,
+    ) {
+        return new UpdateSchedulingOrderRoute(
+            "/orders/update-scheduling/:id",
             HttpMethod.PUT,
-            updateOrderUsecase,
+            updateSchedulingOrderUsecase,
         )
     }
 
     public getHandler(): (req: Request, res: Response) => Promise<void> {
         return async (req: Request, res: Response) => {
             const { id } = req.params
-            await this.updateOrderUsecase.execute({
-                id,
-                ...req.body,
-            })
+            await this.updateSchedulingOrderUsecase.execute({ id, ...req.body })
 
             res.status(200).send()
         }
@@ -45,6 +44,6 @@ export class UpdateOrderRoute implements Route {
         response: Response,
         next: NextFunction,
     ) => Promise<void>)[] {
-        return [AuthMiddleware(), Validation(updateOrderDto)]
+        return [AuthMiddleware(), Validation(updateSchedulingOrderDto)]
     }
 }
