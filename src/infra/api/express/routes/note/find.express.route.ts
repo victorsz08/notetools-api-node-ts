c620357ac89c50878442555d49f628bc9c5225f3
Victor Siqueira <131918226+victorsz08@users.noreply.github.com>
2025-07-26 15:09:32 +0000

    feat: add find note express route

---
+++ src/infra/api/express/routes/note/find.express.route.ts
@@ -0,0 +1,41 @@
+import { FindNoteUsecase } from "@/usecases/note/find.usecase"
+import { HttpMethod, Route } from "../route.express"
+import { Request, Response, NextFunction } from "express"
+import { AuthMiddleware } from "@/middleware/auth.middleware"
+
+export class FindNoteRoute implements Route {
+    private constructor(
+        private readonly path: string,
+        private readonly method: HttpMethod,
+        private readonly findNoteUsecase: FindNoteUsecase,
+    ) {}
+
+    public static build(findNoteUsecase: FindNoteUsecase) {
+        return new FindNoteRoute("/notes/:id", HttpMethod.GET, findNoteUsecase)
+    }
+
+    public getHandler(): (req: Request, res: Response) => Promise<void> {
+        return async (req: Request, res: Response) => {
+            const { id } = req.params
+            const output = await this.findNoteUsecase.execute({ id })
+
+            res.status(200).send(output)
+        }
+    }
+
+    public getPath(): string {
+        return this.path
+    }
+
+    public getMethod(): HttpMethod {
+        return this.method
+    }
+
+    public getMiddleware(): ((
+        request: Request,
+        response: Response,
+        next: NextFunction,
+    ) => Promise<void>)[] {
+        return [AuthMiddleware()]
+    }
+}
