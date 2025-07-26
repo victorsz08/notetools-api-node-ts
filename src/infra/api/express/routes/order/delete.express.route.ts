7c7076ba86eaba720ca4ace3e17cf7a4b9e1c357
victorsz08 <joaovictorsal777@gmail.com>
2025-07-26 06:50:21 -0300

    feat: add delete order express route

---
+++ src/infra/api/express/routes/order/delete.express.route.ts
@@ -0,0 +1,45 @@
+import { DeleteOrderUsecase } from "./../../../../../usecases/order/delete.usecase"
+import { Request, Response, NextFunction } from "express"
+import { HttpMethod, Route } from "../route.express"
+import { AuthMiddleware } from "@/middleware/auth.middleware"
+
+export class DeleteOrderRoute implements Route {
+    private constructor(
+        private readonly path: string,
+        private readonly method: HttpMethod,
+        private readonly deleteOrderUsecase: DeleteOrderUsecase,
+    ) {}
+
+    public static build(deleteOrderUsecase: DeleteOrderUsecase) {
+        return new DeleteOrderRoute(
+            "/orders/:id",
+            HttpMethod.DELETE,
+            deleteOrderUsecase,
+        )
+    }
+
+    public getHandler(): (req: Request, res: Response) => Promise<void> {
+        return async (req: Request, res: Response) => {
+            const { id } = req.params
+            await this.deleteOrderUsecase.execute({ id })
+
+            res.status(200).send()
+        }
+    }
+
+    public getPath(): string {
+        return this.path
+    }
+
+    public getMethod(): HttpMethod {
+        return this.method
+    }
+
+    public getMiddleware(): ((
+        request: Request,
+        response: Response,
+        next: NextFunction,
+    ) => Promise<void>)[] {
+        return [AuthMiddleware()]
+    }
+}
