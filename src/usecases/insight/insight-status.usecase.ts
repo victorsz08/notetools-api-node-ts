9dd7f7e447e6b718478a4f30cf12e99da18f029f
victorsz08 <joaovictorsal777@gmail.com>
2025-07-27 08:26:30 -0300

    feat: add get insight status usecase

---
+++ src/usecases/insight/insight-status.usecase.ts
@@ -0,0 +1,36 @@
+import { InsightInterface } from "@/domain/interface/insight.interface"
+import { Usecase } from "../usecase.core"
+
+export type GetInsightStatusInput = {
+    userId: string
+    startDate: Date
+    endDate: Date
+}
+
+export type GetInsightStatusOutput = {
+    pending: number
+    connected: number
+    canceled: number
+}
+
+export class GetInsightStatusUsecase
+    implements Usecase<GetInsightStatusInput, GetInsightStatusOutput>
+{
+    private constructor(private readonly insightInterface: InsightInterface) {}
+
+    public static build(insightInterface: InsightInterface) {
+        return new GetInsightStatusUsecase(insightInterface)
+    }
+    public async execute(
+        input: GetInsightStatusInput,
+    ): Promise<GetInsightStatusOutput> {
+        const { userId, startDate, endDate } = input
+        const insightStatus = await this.insightInterface.getInsightStatus(
+            userId,
+            startDate,
+            endDate,
+        )
+
+        return insightStatus
+    }
+}
