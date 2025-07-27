b71a8d24d73eb79c09044909edcadb863cfa744c
victorsz08 <joaovictorsal777@gmail.com>
2025-07-27 08:07:48 -0300

    fix: add sub and add months in Date Pattern

--- src/patterns/date.ts
+++ src/patterns/date.ts
@@ -10,4 +10,12 @@ export class DatePattern {
             .tz("America/Sao_Paulo")
             .format("YYYY-MM-DD HH:mm:ss")
     }
+
+    public static addMonth(date: Date, month: number): Date {
+        return moment(date).add(month, "month").toDate()
+    }
+
+    public static subMonth(date: Date, month: number): Date {
+        return moment(date).subtract(month, "month").toDate()
+    }
 }
