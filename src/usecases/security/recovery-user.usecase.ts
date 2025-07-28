138d0153cac9d0c2612c3ef10ce756e9e9c1d8fc
victorsz08 <joaovictorsal777@gmail.com>
2025-07-28 12:12:35 -0300

    feat: add recovery password usecase

---
+++ src/usecases/security/recovery-user.usecase.ts
@@ -0,0 +1,53 @@
+import { UserInterface } from "@/domain/interface/user.interface"
+import { Usecase } from "../usecase.core"
+import {
+    HttpException,
+    HttpStatusCode,
+} from "@/package/exceptions/http-exceptions"
+import { Hash } from "@/patterns/hash"
+import { DatePattern } from "@/patterns/date"
+
+export type RecoveryUserInput = {
+    id: string
+}
+
+export type RecoveryUserOutput = {
+    newPassword: string
+}
+
+export class RecoveryUserUsecase
+    implements Usecase<RecoveryUserInput, RecoveryUserOutput>
+{
+    private constructor(private readonly userInterface: UserInterface) {}
+
+    public static build(userInterface: UserInterface) {
+        return new RecoveryUserUsecase(userInterface)
+    }
+
+    public async execute(
+        input: RecoveryUserInput,
+    ): Promise<RecoveryUserOutput> {
+        const { id } = input
+        const user = await this.userInterface.find(id)
+        const updatedAt = DatePattern.getCurrentDate()
+
+        if (!user) {
+            throw new HttpException(
+                HttpStatusCode.NOT_FOUND,
+                "Usuário não localizado com esse id",
+            )
+        }
+
+        const randomPassword = Math.random().toString(30).slice(-10)
+        const newPasswordHashed = await Hash.hash(randomPassword)
+
+        await this.userInterface.changePassword(
+            id,
+            newPasswordHashed,
+            updatedAt,
+        )
+        return {
+            newPassword: randomPassword,
+        }
+    }
+}
