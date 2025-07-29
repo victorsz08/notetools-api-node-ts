cc70cb6c7ac5db61e12266ce6ac4e83df9e512ee
victorsz08 <joaovictorsal777@gmail.com>
2025-07-29 13:10:30 -0300

    feat: add grant user role usecase

---
+++ src/usecases/security/grant-user.usecase.ts
@@ -0,0 +1,39 @@
+import { Role } from "@/domain/enum/role.enum"
+import { Usecase } from "../usecase.core"
+import { UserInterface } from "@/domain/interface/user.interface"
+import {
+    HttpException,
+    HttpStatusCode,
+} from "@/package/exceptions/http-exceptions"
+
+export type GrantUserInput = {
+    id: string
+    role: Role
+}
+
+export type GrantUserOutput = void
+
+export class GrantUserUsecase
+    implements Usecase<GrantUserInput, GrantUserOutput>
+{
+    private constructor(private readonly userRepository: UserInterface) {}
+
+    public static build(userRepository: UserInterface) {
+        return new GrantUserUsecase(userRepository)
+    }
+
+    public async execute(input: GrantUserInput): Promise<void> {
+        const { id, role } = input
+        const user = await this.userRepository.find(id)
+
+        if (!user) {
+            throw new HttpException(
+                HttpStatusCode.NOT_FOUND,
+                "Usuário não encontrado com esse id",
+            )
+        }
+
+        await this.userRepository.grantUser(id, role)
+        return
+    }
+}
